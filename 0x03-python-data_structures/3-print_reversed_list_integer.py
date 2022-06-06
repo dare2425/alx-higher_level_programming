c1867e1775a600201737b4cef6619c96a45450ca
dare2425 <shallhaile11@gmail.com>
2022-06-06 10:17:08 -0300

    My commit

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
-    """Replace an element of a list at a specific position"""
-    if idx >= 0 and idx < len(my_list):
-        my_list[idx] = element
-    return (my_list)
+def print_reversed_list_integer(my_list=[]):
+    """Print all integers of a list in reverse order."""
+    if isinstance(my_list, list):
+        my_list.reverse()
+        for i in my_list:
+            print("{:d}".format(i))
