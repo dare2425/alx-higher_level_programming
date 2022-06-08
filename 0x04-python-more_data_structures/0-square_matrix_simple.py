0f7279241154b8952e4dd1fd613ebd6b8eb97b01
dare2425 <shallhaile11@gmail.com>
2022-06-08 08:47:26 -0300

    My commit

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=None):
+    if matrix is None:
+        matrix = []
+    new_matrix = []
+    for i in range(len(matrix)):
+        row = []
+        new_matrix.append(row)
+        for j in range(len(matrix[i])):
+            element = matrix[i][j] ** 2
+            row.append(element)
+    return new_matrix
